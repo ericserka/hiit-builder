f25665ce53c59811d33c3bd3802fcdcca202afeb
ericserka <ericserka@hotmail.com>
2024-09-26 01:05:39 -0300

    feat: improvments in workout screen
    keeping screen awake and pause/resume pressable

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,13 +1,17 @@
 import { dayjs } from '../helpers/dayjs'
 import { useEffect, useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 import * as Progress from 'react-native-progress'
 import { useStore } from '../helpers/store'
 import * as Speech from 'expo-speech'
+import { useKeepAwake } from 'expo-keep-awake'
 
 export const Workout = () => {
   const { sets, currSet, nextSet, endWorkout } = useStore()
   const [secondsLeft, setSecondsLeft] = useState(10);
+  const [timerRunning, setTimerRunning] = useState(true)
+
+  useKeepAwake()
 
   useEffect(() => {
     setSecondsLeft(currSet.duration)
@@ -39,12 +43,20 @@ export const Workout = () => {
       Speech.speak(`${secondsLeft}`, { language: "en" })
     }
 
-    const intervalId = setInterval(() => {
-      setSecondsLeft((prevTime) => prevTime - 1);
-    }, 1000);
+    let intervalId
+
+    if (timerRunning) {
+      intervalId = setInterval(() => {
+        setSecondsLeft((prevTime) => prevTime - 1);
+      }, 1000);
+    }
 
     return () => clearInterval(intervalId);
-  }, [secondsLeft]);
+  }, [secondsLeft, timerRunning]);
+
+  const pauseResumeTimer = () => {
+    setTimerRunning((prev) => !prev)
+  }
 
   return (
     <View>
@@ -58,6 +70,9 @@ export const Workout = () => {
         showsText
       />
       <View style={styles.speedView}>
+        <Pressable onPress={pauseResumeTimer} style={styles.pressable}>
+          <Text style={{ ...styles.text, color: 'white' }}>{timerRunning ? 'Pause' : 'Resume'}</Text>
+        </Pressable>
         <Text style={styles.text}>Current speed</Text>
         <Text style={styles.currSpeed}>{currSet.speed} km/h</Text>
         {currSet.id !== 9 && <Text style={styles.text}>next {sets[currSet.id + 1].speed} km/h</Text>}
@@ -80,5 +95,12 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 32
+  },
+  pressable: {
+    backgroundColor: "blue",
+    fontWeight: "bold",
+    height: 55,
+    padding: 10,
+    alignItems: "center"
   }
 })
